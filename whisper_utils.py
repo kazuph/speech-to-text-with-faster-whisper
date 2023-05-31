681e0e538cb160b976743ad5f31b9aff7501183a
Kazuhiro Homma <kazu.homma@gmail.com>
2023-05-31 16:41:07 +0900

    rye対応&Mac対応

--- whisper_utils.py
+++ whisper_utils.py
@@ -1,15 +1,21 @@
 from faster_whisper import WhisperModel
+import platform
 
 
 class WhisperModelWrapper:
     def __init__(self):
         self.model_size_or_path = "large-v2"
-        self.model = WhisperModel(
-            self.model_size_or_path, device="cuda", compute_type="float16"
-        )
+        if platform.system() == "Darwin":
+            self.model = WhisperModel(
+                self.model_size_or_path, device="cpu", compute_type="int8"
+            )
+        else:
+            self.model = WhisperModel(
+                self.model_size_or_path, device="cuda", compute_type="float16"
+            )
 
     def transcribe(self, audio):
         segments, _ = self.model.transcribe(
-            audio=audio, beam_size=3, language="ja", without_timestamps=True, 
+            audio=audio, beam_size=3, language="ja", without_timestamps=True,
         )
         return segments
